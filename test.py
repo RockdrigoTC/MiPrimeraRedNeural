034543dc0287b9c4257952eb40be4b6b3c9d43c1
Rodrigo <torrealbacruzrodrigo@gmail.com>
2023-04-11 14:49:26 -0600

    Round-Result

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
-import numpy as np
 import pandas as pd
 
 # Cargar los datos de prueba desde un archivo CSV
-# x_test = pd.read_csv('x_test.csv')
-x_test = pd.read_csv('x_test.csv', header=0, delimiter=',')
+x_test = pd.read_csv('x_test.csv')
 
 # Crear la red neuronal secuencial
 model = tf.keras.Sequential()
@@ -19,4 +17,8 @@ model.load_weights('model_weights.h5')
 # Predecir las salidas para los datos de prueba
 y_pred = model.predict(x_test)
 
+
+print("Resultados de la predicción:")
 print(y_pred)
+print("Resultados de la predicción redondeados:")
+print(y_pred.round().astype(int))
